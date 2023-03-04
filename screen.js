dd51deb469cfa2d9f8d81aff859bc5b72c25cfab
shee-la <sheilayvonne45@gmail.com>
2023-03-04 16:44:45 +0300

    changed some few lines to simpler ones

--- screen.js
+++ screen.js
@@ -2,8 +2,9 @@ const previewElem = document.getElementById("preview");
 const button = document.querySelector("button");
 
 button.addEventListener("click", async () => {
-previewElem.srcObject = await navigator.mediaDevices.getDisplayMedia({
-    video:{cusor:"always"},
-     audio: false,
-});
+    const options = {
+        video: {cusor: "always"},
+        audio: false,
+    };
+previewElem.srcObject = await navigator.mediaDevices.getDisplayMedia(options);
 });
